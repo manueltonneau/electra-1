f21f3ae5bd0ca2d52cedfd47e2b004a6b49877f2
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-11-06 11:12:00 +0100

    Add Eduardo's modifications

--- configure_pretraining.py
+++ configure_pretraining.py
@@ -62,6 +62,7 @@ class PretrainingConfig(object):
     self.embedding_size = None  # bert hidden size by default
     self.vocab_size = 30522  # number of tokens in the vocabulary
     self.do_lower_case = True  # lowercase the input?
+    self.init_checkpoint= None
 
     # generator settings
     self.uniform_generator = False  # generator is uniform at random
